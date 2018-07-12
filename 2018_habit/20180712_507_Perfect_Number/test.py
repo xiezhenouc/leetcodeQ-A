635373c66def4d01e98a740724de74fdf68758a0
xiezhen <xiezhenouc@163.com>
2018-07-12 22:10:26 +0800

    20180712

---
+++ 2018_habit/20180712_507_Perfect_Number/test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import math
+
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 1:
+            return False
+        factor_set = set()
+        factor_set.add(1)
+
+        factor_bound = int(math.sqrt(num)) + 1
+        for i in range(2, factor_bound + 1):
+            if num % i == 0:
+                factor_set.add(i)
+                factor_set.add(num / i)
+
+        return sum(factor_set) == num
+        
+
+obj = Solution()
+num = 100000000
+print obj.checkPerfectNumber(num)
+
+#for num in range(0, 100000):
+#    if obj.checkPerfectNumber(num):
+#        print num
+#
